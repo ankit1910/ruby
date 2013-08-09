98ddb384ae29131dbd402553f3913ac74f006eba
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-08-09 19:11:34 +0530

    account balance

---
+++ accountBalance/accountBalanceCode.rb
@@ -0,0 +1,10 @@
+require_relative 'accountBalance.rb'
+var = Customer.new("ankit",1000)
+puts var.accountNum
+var1 = Customer.new("rahul",1000)
+puts var1.accountNum
+var2 = Customer.new("jaitn",1000)
+puts var2.accountNum
+var3 = Customer.new("sahil",1000)
+puts var3.accountNum
+
