3b236c22a1f0a36433653025736082129c2b19ed
ankit.bansal001@yahoo.comclear <root@vinsol-dell-1525-silver.(none)>
2013-08-13 14:32:50 +0530

    hash inject

---
+++ hash_inject/lib/array.rb
@@ -0,0 +1,16 @@
+class Array
+  def to_hash
+     hash = self.group_by{|ele| ele.to_s.length}
+     hash.inject({}) do |hash, item|
+      len = item[0]
+      if len.even?
+        hash["even"] ||= []
+        hash["even"] << item[1]
+      else
+        hash["odd"] ||= []
+        hash["odd"] << item[1]
+      end
+      hash
+    end
+  end
+end
