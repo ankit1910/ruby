8e1d06b365d10f72a7218c69f5203bc2826573d0
ankit1910 <ankit1910@gmail.com>
2013-08-17 00:30:58 +0530

    elements other than integer is taken care

---
+++ power/lib/array.rb
@@ -0,0 +1,8 @@
+class Array
+  def power(exponent)
+    self.map! do |element|
+      (element.is_a? Numeric) ? element **= exponent : break 
+    end
+  end
+end
+ 
--- power/main.rb
+++
@@ -1,6 +0,0 @@
-class Array
-  def power(pow)
-    self.map!{|num| num ** pow }
-  end
-end
-p [2, 4, 5].power(3) 
