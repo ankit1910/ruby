5e692b05170f499ff250117a52a2a4eda2bac762
ankit1910 <ankit1910@gmail.com>
2013-08-17 00:07:52 +0530

    extra method removed

--- vehicle_sub/lib/vehicle.rb
+++ vehicle_sub/lib/vehicle.rb
@@ -1,26 +1,17 @@
 # super class Vehicle
 class Vehicle
+  attr_reader :name
+  attr_accessor :price
   def  initialize(name, price)
     @name = name
     @price = price    
   end
-  def name
-    @name
-  end
-  def price
-    @price
-  end
-  def price=(newPrice)
-    @price = newPrice
-  end
 end
 # sub class bike with super class as Vehicle
 class Bike < Vehicle
+  attr_reader :dealer
   def initialize(dealer, name, price)
     super(name, price)
     @dealer = dealer
-  end
-  def dealer
-    @dealer
   end
 end
