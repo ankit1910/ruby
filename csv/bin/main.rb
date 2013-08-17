189950082dbda6ad934db8a35488cc70bbd1cbaa
ankit1910 <ankit1910@gmail.com>
2013-08-18 00:59:03 +0530

    csv method used instead of hash

--- csv/bin/main.rb
+++ csv/bin/main.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/read_write_csv.rb'
 require 'csv'
 object = ReadWriteCsv.new
-object.read_csv
-object.write_csv
+object.read_csv("#{File.dirname(__FILE__)}/data.csv")
+object.write_csv("#{File.dirname(__FILE__)}/data_output.csv")
 
--- csv/lib/read_write_csv.rb
+++ csv/lib/read_write_csv.rb
@@ -2,14 +2,14 @@ class ReadWriteCsv
   def initialize
     @file_data_hash = Hash.new {|hash, key| hash[key] = []}
   end
-  def read_csv    
-    arr_of_arrs = CSV.read("#{File.dirname(__FILE__)}/../bin/data.csv")
+  def read_csv(file)   
+    arr_of_arrs = CSV.read(file)
     arr_of_arrs.each do |element|
       @file_data_hash[element[2]] << [element[0], element[1]]
     end
   end
-  def write_csv
-    CSV.open("#{File.dirname(__FILE__)}/../bin/data_output.csv", "wb") do |csv|
+  def write_csv(file)
+    CSV.open(file, "wb") do |csv|
       @file_data_hash.each do |key, value|
         csv << [key]
         @file_data_hash[key].each do |element|
