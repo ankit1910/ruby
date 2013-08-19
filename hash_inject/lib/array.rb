abc72f2e5f3e880a2ce4137df97b3756afb4bb67
ankit1910 <ankit1910@gmail.com>
2013-08-19 11:01:21 +0530

    hardcoding eleminated

--- hash_inject/lib/array.rb
+++ hash_inject/lib/array.rb
@@ -1,8 +1,7 @@
 class Array
   def to_hash
     output_hash = self.group_by{|ele| (ele.is_a? Enumerable) ? ele.size : ele.to_s.length}
-    new_hash = {"odd" => [], "even" => []}
-    output_hash.inject(new_hash) do |hash, item|
+    output_hash.inject(Hash.new { |hash, key| hash[key] =[]}) do |hash, item|
       len = item[0]
       len.even? ? hash["even"] << item[1] : hash["odd"] << item[1]
       hash
