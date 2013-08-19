82d3b620b01efa6a90bf0ec26ca64b37c2bbd911
ankit1910 <ankit1910@gmail.com>
2013-08-19 16:19:57 +0530

    in one method

--- highlight/lib/string.rb
+++ highlight/lib/string.rb
@@ -1,7 +1,7 @@
 class String
   def count_no_of_occurence(search_word)
-    self.gsub!(/#{search_word}/i,"(#{search_word})")
-    count = self.scan(/#{search_word}/i).size
+  	count = 0
+    k = self.gsub!(/#{search_word}/i){ |match| count += 1 ;"(#{match})" }
     return self, count 
   end
 end
