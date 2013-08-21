1896b4e3b216a35c919dd73bb1f5a823f7b60c83
ankit1910 <ankit1910@gmail.com>
2013-08-21 21:35:11 +0530

    function name change

--- highlight/bin/main.rb
+++ highlight/bin/main.rb
@@ -2,6 +2,6 @@ require_relative '../lib/string.rb'
 print "enter string : "
 input_string = gets.chomp
 print "word that to be highlighted : "
-search_word = gets.chomp
-output = input_string.count_no_of_occurence(search_word)
+word_to_highlight = gets.chomp
+output = input_string.highlight_word(word_to_highlight)
 puts "#{output[0]} \nTotal Occurences found: #{output[1]}"
--- highlight/lib/string.rb
+++ highlight/lib/string.rb
@@ -1,7 +1,7 @@
 class String
-  def count_no_of_occurence(search_word)
-  	count = 0
-    k = self.gsub!(/#{search_word}/i){ |match| count += 1 ;"(#{match})" }
+  def highlight_word(word)
+    count = 0
+    k = self.gsub!(/#{word}/i){ |match| count += 1 ;"(#{match})" }
     return self, count 
   end
 end
