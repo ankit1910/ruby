8dbde4f054e0fd9a11a0829f712812d7f2c37488
ankit1910 <ankit1910@gmail.com>
2013-08-21 13:16:21 +0530

    changes for n argument

--- sum_time_mul/bin/main.rb
+++ sum_time_mul/bin/main.rb
@@ -1,15 +1,2 @@
 require_relative '../lib/time.rb'
-flag = 0
-input = '("11:23:07","22:53:45","0:23:23","23:45:56")'
-regex = /([0-2]?[0-9]):([0-6]?[0-9]):([0-6]?[0-9])/ 
-time = input.split(',')
-time.map! do |ele|
-  if ele =~ regex
-    ele = Time.new(0,1,1,$1.to_i,$2.to_i,$3.to_i)
-  else
-    puts "wrong format"
-    flag = 1
-    break
-  end
-end
-time[0].add_time(time[1 .. -1]) if time.size > 1 && flag != 1
+TimeAdd.add_time("11:23:07", "22:53:45", "0:23:23", "23:45:56")
--- sum_time_mul/lib/time.rb
+++ sum_time_mul/lib/time.rb
@@ -1,12 +1,19 @@
-class Time
-  def add_time(time_array)
-    result_time = self
-    size = time_array.size
-    size.times do |i|
-      add_factor = (time_array[i].hour * 3600) + (time_array[i].min * 60) + time_array[i].sec
-      result_time = result_time + add_factor
+require 'time'
+module TimeAdd
+  def self.add_time(*times)
+    begin
+      result_time = Time.new("00 : 00 : 00")
+      day = result_time.day
+      times.each do |time|
+        time = Time.parse(time)
+        add_factor = (time.hour * 3600) + (time.min * 60) + time.sec
+        result_time += add_factor
+      end
+      day = result_time.day - day
+      printf("%02d days & %02d : %02d : %02d\n",day, result_time.hour, result_time.min, result_time.sec)
+    rescue ArgumentError
+      puts "wrong format of time"
     end
-    day = result_time.day - self.day
-    printf("%02d days & %02d : %02d : %02d\n",day, result_time.hour, result_time.min, result_time.sec)    
+        
   end
 end
