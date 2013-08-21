7c7608bb5cf73ee78cc23dad44a6a7a83eedb5e9
ankit1910 <ankit1910@gmail.com>
2013-08-21 16:58:05 +0530

    various rescue added

--- factorial_exception/bin/main.rb
+++ factorial_exception/bin/main.rb
@@ -1,9 +1,19 @@
 require_relative '../lib/integer.rb'
+class NegativeNumber < StandardError
+end
+class StringInput < StandardError
+end
 puts "enter number"
 begin
-  number = Integer(gets)
-  raise Exception if number < 0
-  p number.factorial
-rescue Exception => e
-  p "wrong value for input for finding the factorial"
+  number = gets.chomp
+  raise NegativeNumber if number.to_i < 0
+  raise StringInput if number =~ /[a-z]/i
+  raise if number !~ /[0-9]+/
+  p number.to_i.factorial
+rescue NegativeNumber
+  p "Negative value for finding the factorial"
+rescue StringInput
+  p "String value for finding the factorial"
+rescue Exception
+  p "the input value is not correct"
 end
