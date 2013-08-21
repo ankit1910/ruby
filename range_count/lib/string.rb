a6fe530611b594f590005bb6927c45851d9636e7
ankit1910 <ankit1910@gmail.com>
2013-08-21 17:05:16 +0530

    optimised

--- range_count/lib/string.rb
+++ range_count/lib/string.rb
@@ -1,12 +1,17 @@
 class String
   def count_diff_char
     hash = Hash.new(0)
+    digit, upcase, downcase = ('0'..'9'), ('A'..'Z'), ('a'..'z')
     self.each_char do |char|
       case char
-        when ('0'..'9') then hash["digit"] += 1
-        when ('A'..'Z') then hash["upcase"] += 1
-        when ('a'..'z') then hash["downcase"] += 1
-        else hash["special symbol"] += 1
+        when digit
+          hash["digit"] += 1
+        when upcase 
+          hash["upcase"] += 1
+        when downcase 
+          hash["downcase"] += 1
+        else 
+          hash["special symbol"] += 1
       end
     end
     hash
