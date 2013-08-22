c882fb461dec237668fdb80c04b81e3eba1b0208
ankit1910 <ankit1910@gmail.com>
2013-08-22 12:23:45 +0530

    user define exception used

--- name_raise/bin/main.rb
+++ name_raise/bin/main.rb
@@ -3,11 +3,8 @@ require_relative '../lib/name_errors.rb'
 begin
   puts "enter first name"
   first_name = gets.chomp
-  raise NameErrors, "First name can not be empty" if first_name.empty?
-  raise NameErrors,"First letter of first name is not capital" if (first_name != first_name.capitalize)
   puts "enter last name"
   last_name = gets.chomp
-  raise NameErrors, "Last name can not be empty" if last_name.empty?
   obj = Name.new(first_name, last_name)
   p obj
 rescue NameErrors => e
--- name_raise/lib/name.rb
+++ name_raise/lib/name.rb
@@ -1,7 +1,10 @@
 class Name
   attr_reader :first_name,:last_name
   def initialize (first_name, last_name)
+    raise NameErrors, "First name can not be empty" if first_name.empty?
+    raise NameErrors,"First letter of first name is not capital" if (first_name != first_name.capitalize)
     @first_name = first_name
+    raise NameErrors, "Last name can not be empty" if last_name.empty?
     @last_name = last_name
   end
   def to_s
