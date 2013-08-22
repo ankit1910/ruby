e990162156da70af636830fb90799c6aafb05e4f
ankit1910 <ankit1910@gmail.com>
2013-08-22 10:48:30 +0530

    user define exception used

--- name_raise/lib/name.rb
+++ name_raise/lib/name.rb
@@ -1,13 +1,10 @@
 class Name
-  attr_accessor :first_name,:last_name
+  attr_reader :first_name,:last_name
   def initialize (first_name, last_name)
-    begin
-      raise 'name field cant be empty' if first_name.length == 0 or last_name.length ==0
-      raise 'first letter of first name is not capital' if first_name !~ /[A-Z][\w]+$/
-      @first_name = first_name
-      @last_name = last_name
-      rescue Exception => e 
-         p e.message
-    end
-   end
+    @first_name = first_name
+    @last_name = last_name
+  end
+  def to_s
+    "Name : #{first_name} #{last_name}"
+  end
 end
---
+++ name_raise/lib/name_errors.rb
@@ -0,0 +1,2 @@
+class NameErrors < StandardError
+end
