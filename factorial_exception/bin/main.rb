44024e4ab1f0973443c8e618e382c78fa8d2135e
ankit1910 <ankit1910@gmail.com>
2013-08-22 22:24:31 +0530

    raised in factorial function

--- factorial_exception/bin/main.rb
+++ factorial_exception/bin/main.rb
@@ -4,6 +4,7 @@ require_relative '../lib/string_input.rb'
 puts "enter number"
 begin
   input = gets.chomp
+  raise Exception if input.to_f != input.to_i
   number = (input =~ /^0+$/) ? 1 : input.to_i
   p number.factorial
 rescue NegativeNumber
