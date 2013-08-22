139e1d49f39a2340edbd9e5f88d824e2c2a55170
ankit1910 <ankit1910@gmail.com>
2013-08-22 22:46:11 +0530

    function moved to def pascal

--- pascal/bin/main.rb
+++ pascal/bin/main.rb
@@ -1,8 +1,2 @@
 require_relative '../lib/pascal.rb'
-Pascal.pascal(6) do |i|
-  x = 1
-  0.upto(i) do |j|
-    print x, " "
-    x = x * (i - j) / (j + 1)
-  end
-end
+Pascal.pascal(6) { |x| print x, " "}
--- pascal/lib/pascal.rb
+++ pascal/lib/pascal.rb
@@ -1,7 +1,11 @@
 module Pascal
   def self.pascal(num)
-    (num+1).times do |i|
-      yield(i)
+    (num + 1).times do |i|
+      x = 1
+      0.upto(i) do |j|
+        yield(x)
+        x = x * (i - j) / (j + 1)
+      end
       puts ""
     end
   end
