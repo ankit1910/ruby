784f0ed7701d6b24aa16c1ce0e8f7ecd69270582
ankit1910 <ankit1910@gmail.com>
2013-08-22 16:10:18 +0530

    days calculation correted

--- sum_time_mul/lib/time_add.rb
+++ sum_time_mul/lib/time_add.rb
@@ -3,14 +3,15 @@ module TimeAdd
   def self.add_time(*times)
     begin
       result_time = Time.new("00 : 00 : 00")
-      day = result_time.day
+      hours = 0
       times.each do |time|
         time = Time.parse(time)
         add_factor = (time.hour * 3600) + (time.min * 60) + time.sec
+        hours += time.hour
         result_time += add_factor
       end
-      day = result_time.day - day
-      printf("%02d days & %02d : %02d : %02d\n",day, result_time.hour, result_time.min, result_time.sec)
+      days = hours / 24
+      printf("%02d days & %02d : %02d : %02d\n",days, result_time.hour, result_time.min, result_time.sec)
     rescue ArgumentError
       puts "wrong format of time"
     end
