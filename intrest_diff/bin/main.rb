be2f1b55ca1daaf8142d02f03e1f7fe08a199639
ankit1910 <ankit1910@gmail.com>
2013-08-30 15:38:45 +0530

    diff function for rate

--- intrest_diff/bin/main.rb
+++ intrest_diff/bin/main.rb
@@ -1,5 +1,10 @@
 require_relative '../lib/interest.rb'
-obj = Interest.new {[10000.0, 4.0]}
-p "simple interest is : #{obj.simple_interest}"
-p "compound interest is : #{obj.compound_interest}"
-p "diffrence in interest is : #{obj.diffrence_interest}"
+obj_array = []
+obj_array << Interest.new { [10000.0, 4.0] }
+obj_array << Interest.new { [20000.0, 2.0] }
+obj_array.each do |object|
+  puts "Details : Amount = '#{ object.principal }' and Time = '#{ object.time }' "
+  puts "simple interest is : #{ object.simple_interest }"
+  puts "compound interest is : #{ object.compound_interest }"
+  puts "diffrence in interest is : #{ object.diffrence_interest }"
+end
