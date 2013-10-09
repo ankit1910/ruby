fcfc5c22162987a07350d438a6e0ccbae961d5f9
root <root@vinsol-dell-1525-silver.(none)>
2013-10-09 12:05:53 +0530

    changes made

--- name_raise/bin/main.rb
+++ name_raise/bin/main.rb
@@ -1,10 +1,11 @@
 require_relative '../lib/name.rb'
 require_relative '../lib/exception.rb'
 begin
+  # strip function checking for blank spaces
   puts "enter first name"
-  first_name = gets.chomp
+  first_name = gets.chomp.strip
   puts "enter last name"
-  last_name = gets.chomp
+  last_name = gets.chomp.strip
   obj = Name.new(first_name, last_name)
   puts obj
 rescue StandardError
--- name_raise/lib/name.rb
+++ name_raise/lib/name.rb
@@ -1,12 +1,11 @@
 class Name
   def initialize (first_name, last_name)
-    case
-    when first_name.empty?
+    if !first_name || first_name.empty?
       raise FirstNameEmptyError, "first name can not be empty"
-    when (first_name != first_name.capitalize)
-      raise FirstNameNotCapitalizeError, "first name should be capitalize"
-    when last_name.empty?
+    elsif !last_name || last_name.empty?
       raise LastNameEmptyError, "last name can not be empty"
+    elsif first_name != first_name.capitalize
+      raise FirstNameNotCapitalizeError, "first name should be capitalize"
     else
       @first_name = first_name
       @last_name = last_name
