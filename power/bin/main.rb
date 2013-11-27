fb8e1b6e1fabc2e7efe9f062163d670629d3772c
ankit1910 <ankit1910@gmail.com>
2013-11-27 17:47:28 +0530

    commit1

--- power/bin/main.rb
+++ power/bin/main.rb
@@ -1,4 +1,3 @@
 require_relative '../lib/array.rb'
 raised_value_of_array = [2.5, 4.3, 5].power(3) 
 p raised_value_of_array ? raised_value_of_array : "the value of input array is unacceptable"
-#sdfvsdf
